1983a828e2652870a9bd8819be1b84c633d84687
ch Nitheesh <chalumuri.nitheesh@gmail.com>
2025-05-18 20:27:06 +0530

    slight changes

--- client/src/components/chat/chat/footer.jsx
+++ client/src/components/chat/chat/footer.jsx
@@ -1,12 +1,24 @@
-import { Box } from "@mui/material"
-import {EmojiEmotionsOutlinedIcon}from '@mui/icons-material/EmojiEmotionsOutlined';
+import { Box,InputBase,styled} from "@mui/material";
+import EmojiEmotionsOutlinedIcon from '@mui/icons-material/EmojiEmotionsOutlined';
 import AttachFileOutlinedIcon from '@mui/icons-material/AttachFileOutlined';
+
+const Container = styled(Box)`
+height:55px;
+background:#ededed;
+`
+
 const Footer =()=>{
     return(
-        <Box>
-            <EmojiEmotionsOutlinedIcon></EmojiEmotionsOutlinedIcon>
-            <AttachFileOutlinedIcon></AttachFileOutlinedIcon>
-        </Box>
+        <><h1>hello</h1></>
+        // <Container>
+        //     <EmojiEmotionsOutlinedIcon></EmojiEmotionsOutlinedIcon>
+        //     <AttachFileOutlinedIcon></AttachFileOutlinedIcon>
+        //     <Box>
+        //         <InputBase
+        //         placeholder=" Type a mesaage! "
+        //         ></InputBase>
+        //     </Box>
+        // </Container>
     )
 }
 
