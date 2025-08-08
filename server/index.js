ee4ed93e9d02cec33fb2d2f2a9d468cfb835ef2d
nitheesh920 <159281733+Nitheesh-Purnanand@users.noreply.github.com>
2025-08-08 23:57:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,14 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
 import Connection from './database/db.js';
 import Routes from './routes/route.js';
-
-
 dotenv.config();
 const app = express();
-
 const PORT = 8000;
 
 const username = process.env.DB_USERNAME;
